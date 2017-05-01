4993ec9b8fee75c7b6a6e996ee4a0f54e2f47c56
asugar13 <asier.ugarteche@gmail.com>
2017-04-30 20:35:29 -0400

    started implementing iterator over heroes array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
+  <h2>My Heroes</h2>
+<ul class="heroes">
+  <li *ngFor="let hero of heroes">
+
+  </li>
+</ul>
   <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
         <div>
@@ -14,9 +20,23 @@ export class AppComponent  {
   title = 'Tour of Heroes';
   hero: Hero = {id: 1,
   name: 'Windstorm'};
+  heroes = HEROES;
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
